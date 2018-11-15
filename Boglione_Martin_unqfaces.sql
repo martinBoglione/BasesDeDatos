eae71a9bd00d3277bb30f9311b9615dbccd2e394
Martin <37816801+martinBoglione@users.noreply.github.com>
2018-11-15 11:59:44 -0300

    Add files via upload

--- Boglione_Martin_unqfaces.sql
+++ Boglione_Martin_unqfaces.sql
@@ -1,12 +1,10 @@
---Martin Boglione
---Tp Sql
-
---3.2
---1
+--3.2 Preguntas
+--1									  
 CREATE TABLE carrera
 			  (id SERIAL PRIMARY KEY,
 			   nombre VARCHAR(200));
 										
+
 CREATE TABLE usuario 
 			  (id SERIAL PRIMARY KEY,
 			   nombre VARCHAR(100),
@@ -56,8 +54,8 @@ CREATE TABLE like_publicacion
 			 fecha TIMESTAMP,
 			 CONSTRAINT like_publicacion_pk PRIMARY KEY(id_public,id_user),
 			 CONSTRAINT usuario_fk FOREIGN KEY (id_user) REFERENCES usuario(id));
-			 CONSTRAINT publicacion_fk FOREIGN KEY (id_public) REFERENCES publicacion(id)
-																			
+			 CONSTRAINT publicacion_fk FOREIGN KEY (id_public) REFERENCES publicacion(id)																
+ 																			
 CREATE TABLE like_comentario
 			  (id_coment INT,
 			   id_user INT,
@@ -65,7 +63,7 @@ CREATE TABLE like_comentario
 			   fecha TIMESTAMP,
 			   CONSTRAINT like_comentario_pk PRIMARY KEY(id_coment,id_user),
 			   CONSTRAINT usuario_fk FOREIGN KEY (id_user) REFERENCES usuario(id));
-			   CONSTRAINT comentario_fk FOREIGN KEY (id_coment) REFERENCES comentario(id)														 
+			   CONSTRAINT comentario_fk FOREIGN KEY (id_coment) REFERENCES comentario(id) 														 
 											
 --2
 ALTER TABLE usuario
@@ -95,7 +93,7 @@ INSERT INTO carrera_usuario
 								
 																				
 --4
---Inserté el set de datos provisto en el archivo datos_unqfaces.sql en otro query(Boglione_Martin_datos.sql)
+--Inserté el set de datos provisto en el archivo datos_unqfaces.sql en otro query para que sea más ordenado este query.
 																				
 --5																				 																			 																				  
 SELECT nombre, COUNT(id_user) AS cant_alu
@@ -108,20 +106,24 @@ ORDER BY length(nombre) DESC;
 SELECT username,nombre,apellido
 FROM (publicacion JOIN usuario ON publicacion.id_user = usuario.id)																	
 													
-																				
+--publicacion con mas de 10 likes
 SELECT id_public,COUNT(positivo)
 FROM like_publicacion
 WHERE positivo = TRUE																		
 GROUP BY id_public
-HAVING COUNT(positivo) > 10		
+HAVING COUNT(positivo) >= 10		
 																				
-
+--id_comentarios que no tienen dislikes
 SELECT id_coment																				
 FROM like_comentario
-WHERE positivo = TRUE
-GROUP BY id_coment
-
+EXCEPT
+SELECT id_coment
+FROM like_comentario
+WHERE positivo=false
+																				
+																				
 --7
+																				
 																				
 --8																	
 SELECT nombre_grupo,COUNT(positivo) as cant_Likes																	
@@ -167,10 +169,9 @@ min(age(fecha_nacimiento)) AS menor,
 avg(age(fecha_nacimiento)) AS promedio
 FROM grupo_usuario 
 JOIN usuario ON grupo_usuario.id_user = usuario.id
-GROUP BY id_grupo;																				
-																
-																				
---13 no terminado
+GROUP BY id_grupo;
+		
+--13
 SELECT DISTINCT(id)
 FROM usuario 
 JOIN carrera_usuario ON usuario.id = carrera_usuario.id_user 
@@ -189,44 +190,70 @@ WHERE usuario.id IN (SELECT id_user
 					 FROM like_comentario
 					 WHERE positivo = true)						
 					  
---14 corregir
-SELECT usuario.id,max(fecha_comentario) AS ultimo_comentario,comentario.contenido
-FROM usuario
-JOIN grupo_usuario ON usuario.id = grupo_usuario.id_user
-JOIN publicacion ON usuario.id = publicacion.id_user
-JOIN comentario ON publicacion.id = comentario.id_public
-GROUP BY usuario.id,comentario.contenido
-ORDER BY usuario.id;
-
---15 no terminado																			  
+--14 hace falta agregar el contenido?????????
+SELECT id_user,max(fecha_comentario) AS ultimo_comentario
+FROM comentario 
+GROUP BY id_user
+ORDER BY id_user; --solo para ver mejor el resultado
+		
+--15 como poner varias carreras en una sola fila																			  
 SELECT usuario.nombre,apellido,username,email,carrera.nombre AS nombre_carreras
 FROM usuario
 JOIN carrera_usuario ON usuario.id = carrera_usuario.id_user
 JOIN carrera ON carrera_usuario.id_carrera = carrera.id
 WHERE usuario.id NOT IN (SELECT id_user
 						 FROM like_comentario)	
+    AND
+	 	
+	usuario.id NOT IN (SELECT id_user
+			     	   FROM like_publicacion)	
+	AND
+	 	
+	usuario.id NOT IN (SELECT id_user
+						 FROM publicacion)
 
-ORDER BY usuario.nombre asc
-				
-		
+	AND
+	 	
+	usuario.id NOT IN (SELECT id_user
+						 FROM comentario);
+
 --16 las comillas son para agregar un espacio entre apellido y age(fecha_nacimiento)
-SELECT nombre, apellido || ' ' || AGE(fecha_nacimiento) AS apellido_y_edad
+SELECT nombre, apellido || '-' || AGE(fecha_nacimiento) AS apellido_y_edad
 FROM usuario;																				  
 																					  																				  
---17
-CREATE VIEW ultima_publicacion
-SELECT username,contenido,last(fecha_publicacion)		
+--17 como ver solo el ultimo comentario al agregar el contenido???
+CREATE VIEW ultima_publicacion AS 
+SELECT username,contenido,max(fecha_publicacion)		
 FROM usuario 
 JOIN publicacion ON usuario.id = publicacion.id_user
 GROUP BY username,contenido	
-ORDER BY username asc		
+ORDER BY username asc;--no hace falta, solo está para ver mejor el resultado
+		
+--18
+
+		
+		
+--19 falta agregar la diferencia de likes y dislikes
+SELECT username,
+COUNT(like_publicacion.positivo = true) AS cant_likes_publicacion, 
+COUNT(like_publicacion.positivo = false) AS cant_dislikes_publicacion, 
+COUNT(like_comentario.positivo = true) AS cant_likes_comentario,	
+COUNT(like_comentario.positivo = false) AS cant_dislikes_comentario,
+		
+( COUNT(like_publicacion.positivo = true) 
+		+
+COUNT(like_comentario.positivo = false) 	) as sumalikes
+		
+		
+FROM usuario	
+JOIN publicacion ON usuario.id = publicacion.id_user
+JOIN comentario ON publicacion.id = comentario.id_public
+JOIN like_publicacion ON publicacion.id = like_publicacion.id_public 
+JOIN like_comentario ON comentario.id = like_comentario.id_coment
+GROUP BY username
+ORDER BY diferencia_likes asc
 		
 		
 		
 		
 		
-		
-		
-		
-		
-																					  
